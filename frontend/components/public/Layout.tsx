60a70eff20a87bee37d668ce614ae1f18ee2a3aa
HiHoi <ghtjd5087@gmail.com>
2023-07-10 18:51:04 +0900

    [FIX]: size fixed

--- frontend/components/public/Layout.tsx
+++ frontend/components/public/Layout.tsx
@@ -6,23 +6,50 @@ import FriendList from "../main/friend_list/FriendList";
 const Layout = () => {
   return (
     <Card sx={{ display: "flex" }}>
-      <Stack sx={{ width: "20vw", height: "100vh", backgroundColor: "yellow" }}>
+      <Stack
+        sx={{
+          width: "20vw",
+          height: "100vh",
+          backgroundColor: "yellow",
+          padding: 0,
+          margin: 0,
+        }}
+      >
         <CardContent sx={{ height: "35vh", backgroundColor: "brown" }}>
           my profile
         </CardContent>
         <FriendList />
       </Stack>
-      <Stack sx={{ width: "60vw", height: "100vh", backgroundColor: "green" }}>
+
+      <Stack
+        sx={{
+          width: "60vw",
+          height: "100vh",
+          backgroundColor: "green",
+          padding: 0,
+          margin: 0,
+        }}
+      >
         <CardContent sx={{ height: "35vh", backgroundColor: "blue" }}>
           game start
         </CardContent>
         <CardContent>chatting window</CardContent>
       </Stack>
-      <CardContent
-        sx={{ width: "20vw", height: "100vh", backgroundColor: "magenta" }}
+
+      <Stack
+        sx={{
+          width: "20vw",
+          height: "100vh",
+          backgroundColor: "magenta",
+          padding: 0,
+          margin: 0,
+        }}
       >
-        room list
-      </CardContent>
+        <CardContent sx={{ height: "35vh", backgroundColor: "purple" }}>
+          room member
+        </CardContent>
+        <CardContent>room list</CardContent>
+      </Stack>
     </Card>
   );
 };
