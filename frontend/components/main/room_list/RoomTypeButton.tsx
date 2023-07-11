a5c7234286f10ea25f3be311d7f9c70ad080788e
jnkeniaem <jeanne.kim9@gmail.com>
2023-07-11 16:47:52 +0900

    [FEAT]: show dm or non dm rooms depends on room type button - todo : protected면 아이콘 같이 보이게하기

--- frontend/components/main/room_list/RoomTypeButton.tsx
+++ frontend/components/main/room_list/RoomTypeButton.tsx
@@ -1,7 +1,7 @@
 // use client;
 
 import { useEffect, useState, useRef } from "react";
-import LockRoundedIcon from '@mui/icons-material/LockRounded';
+import LockRoundedIcon from "@mui/icons-material/LockRounded";
 import List from "./List";
 
 // dm 이냐 아니냐
@@ -11,10 +11,10 @@ import List from "./List";
 //     V = protected
 
 export interface IChatRoom {
-  channelIdx : number,
-  participants : Array<string>,
-  channelType : chatRoomType,
-  password : string,
+  channelIdx: number;
+  participants: Array<string>;
+  channelType: chatRoomType;
+  password: string;
 }
 
 export enum chatRoomType {
@@ -24,102 +24,115 @@ export enum chatRoomType {
 
 export const mockChatRoomList: IChatRoom[] = [
   {
-    channelIdx : 0,
+    channelIdx: 0,
     participants: ["hoslim"],
     channelType: chatRoomType.dm,
-    password : "",
+    password: "",
   },
   {
-    channelIdx : 1, // dm은 channelIdx !dm이랑 따로 한다 했나?
+    channelIdx: 1, // dm은 channelIdx !dm이랑 따로 한다 했나?
     participants: ["jeekim"],
     channelType: chatRoomType.nonDm,
-    password : "asdf",
+    password: "asdf",
   },
   {
-    channelIdx : 2,
+    channelIdx: 2,
     participants: ["jaekim", "haryu", "wochae"],
     channelType: chatRoomType.nonDm,
-    password : "qwer",
+    password: "qwer",
   },
   {
-    channelIdx : 3,
+    channelIdx: 3,
     participants: ["hoslimhoslim1231231231231231231231231"],
     channelType: chatRoomType.nonDm,
-    password : "",
+    password: "",
   },
   {
-    channelIdx : 4,
-    participants: ["hoslim"],
+    channelIdx: 4,
+    participants: ["2hoslim"],
     channelType: chatRoomType.dm,
-    password : "",
+    password: "",
   },
   {
-    channelIdx : 5, // dm은 channelIdx !dm이랑 따로 한다 했나?
-    participants: ["jeekim"],
+    channelIdx: 5, // dm은 channelIdx !dm이랑 따로 한다 했나?
+    participants: ["2jeekim"],
     channelType: chatRoomType.nonDm,
-    password : "asdf",
+    password: "asdf",
   },
   {
-    channelIdx : 6,
-    participants: ["jaekim", "haryu", "wochae"],
+    channelIdx: 6,
+    participants: ["2jaekim", "haryu", "wochae"],
     channelType: chatRoomType.nonDm,
-    password : "qwer",
+    password: "qwer",
   },
   {
-    channelIdx : 7,
-    participants: ["hoslimhoslim1231231231231231231231231"],
+    channelIdx: 7,
+    participants: ["2hoslimhoslim1231231231231231231231231"],
     channelType: chatRoomType.nonDm,
-    password : "",
+    password: "",
   },
 ];
 
-
 export default function RoomTypeButton() {
-  const [rooms, setRooms] = useState<IChatRoom[]>([]);
+  const [nonDmrooms, setNonDmRooms] = useState<IChatRoom[]>([]);
+  const [dmRooms, setDmRooms] = useState<IChatRoom[]>([]);
   const [nonDmDisabled, setNonDmDisabled] = useState(true);
   const [dmDisabled, setDmDisabled] = useState(false);
-  // const [roomType, setRoomType] = useState(true); // true - !dm, false - dm
   const roomTypeRef = useRef(true);
-  useEffect(() => {
+  const DivideRoom = () => {
     mockChatRoomList.map((room, idx) => {
-      console.log("rooms : ", rooms);
       if (room.channelType != chatRoomType.dm) {
-        console.log("room : ", room);
-        setRooms((prev) => {return [...prev, room];});
-        console.log("after setRooms rooms : ", rooms);
-    }
-    })
-  },[]);
+        setNonDmRooms((prev) => {
+          return [...prev, room];
+        });
+      } else {
+        setDmRooms((prev) => {
+          return [...prev, room];
+        });
+      }
+    });
+  }
+  useEffect(() => {
+    DivideRoom();
+  }, []);
   const NonDmBtnClick = () => {
-    setRooms([]);
+    setNonDmRooms([]);
+    setDmRooms([]);
+    DivideRoom();
+    
     roomTypeRef.current = true;
     setNonDmDisabled((prev) => !prev);
     setDmDisabled(false);
-    console.log("hi");
-    mockChatRoomList.map((room, idx) => {
-      console.log("rooms : ", rooms);
-      if (room.channelType != chatRoomType.dm) {
-        console.log("room : ", room);
-        setRooms((prev) => {return [...prev, room];});
-        console.log("after setRooms rooms : ", rooms);
-    }
-    })
-
-  }
+  };
   const DmBtnClick = () => {
-    setRooms([]);
+    setNonDmRooms([]);
+    setDmRooms([]);
+    DivideRoom();
+
     roomTypeRef.current = false;
     setNonDmDisabled(false);
     setDmDisabled(true);
-  }
+  };
 
   return (
     <>
       <div>
-        <button className="notdm typebutton" onClick={NonDmBtnClick} disabled={nonDmDisabled}>Public / Protected</button>
-        <button className="dm typebutton" onClick={DmBtnClick} disabled={dmDisabled}>DM</button>
+        <button
+          className="notdm typebutton"
+          onClick={NonDmBtnClick}
+          disabled={nonDmDisabled}
+        >
+          Public / Protected
+        </button>
+        <button
+          className="dm typebutton"
+          onClick={DmBtnClick}
+          disabled={dmDisabled}
+        >
+          DM
+        </button>
       </div>
-      <List roomsProp={rooms} channelType={roomTypeRef.current}/>
+      <List roomsProp={roomTypeRef.current ? nonDmrooms : dmRooms} channelType={roomTypeRef.current} />
     </>
   );
 }
