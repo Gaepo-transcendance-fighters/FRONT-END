0610d07254071e1a15f8f413e517fc5df7cd76e6
jnkeniaem <jeanne.kim9@gmail.com>
2023-07-11 16:50:34 +0900

    [FEAT] : clicking dm btn, room creating btn(+) is hidden

--- frontend/components/main/room_list/List.tsx
+++ frontend/components/main/room_list/List.tsx
@@ -7,10 +7,9 @@ import {IChatRoom} from "./RoomTypeButton";
 import "@/components/main/room_list/RoomList.css";
 
 export default function List({roomsProp, channelType}:{roomsProp :IChatRoom[],channelType:boolean }) {
-  console.log("In List!!! : ", channelType);
   return (
     <div className="list">
-      <button className="add">+</button>
+      {channelType ? <button className="add">+</button> : ""}
       {
       roomsProp.map((room) => {
         return <button className="item">{room.participants}</button>;
