04a7eed9b57771bb6752635650651c0d5eea0c44
jnkeniaem <jeanne.kim9@gmail.com>
2023-07-12 15:18:58 +0900

    [FEAT]: imported modal

---
+++ frontend/components/main/room_list/CreateRoom.tsx
@@ -0,0 +1,8 @@
+import "@/components/main/room_list/RoomList.css";
+import CreateRoomModal from "./CreateRoomModal";
+
+export default function CreateRoom() {
+  return <>
+	<CreateRoomModal />
+  </>;
+}
--- frontend/components/main/room_list/List.tsx
+++ frontend/components/main/room_list/List.tsx
@@ -2,7 +2,23 @@
 import { IChatRoom } from "./RoomTypeButton";
 import LockRoundedIcon from "@mui/icons-material/LockRounded";
 import "@/components/main/room_list/RoomList.css";
-import Tooltip from "@mui/material/Tooltip";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function List({
   roomsProp,
@@ -11,22 +27,53 @@ export default function List({
   roomsProp: IChatRoom[];
   channelType: boolean;
 }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const leftPadding = (idx: number) => {
     if (idx < 10) return "00" + idx.toString();
     else if (idx < 100) return "0" + idx.toString();
-    else
-      return idx;
+    else return idx;
   };
   return (
     <div className="list">
-      {channelType ? <button className="add">+</button> : ""}
+      {channelType ? (
+        <>
+          <button className="add" onClick={handleOpen}>
+            +
+          </button>
+          <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+        </>
+      ) : (
+        ""
+      )}
       {roomsProp.map((room) => {
         return (
           <button className="item">
             <div className="roomidx">{leftPadding(room.channelIdx)}</div>
             <div className="owner">{room.owner}'s</div>
-            <div className="lock">{room.password == "" ? "" : <LockRoundedIcon sx={{ height: "13px" }} />}</div>
+            <div className="lock">
+              {room.password == "" ? (
+                ""
+              ) : (
+                <LockRoundedIcon sx={{ height: "13px" }} />
+              )}
+            </div>
           </button>
         );
       })}
