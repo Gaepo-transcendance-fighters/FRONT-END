75b447066a26d31f1f968176ed3f5b0410c82806
jnkeniaem <jeanne.kim9@gmail.com>
2023-07-18 18:07:52 +0900

    [FEAT] : added x button

--- frontend/components/main/room_list/ProtectedModal.tsx
+++ frontend/components/main/room_list/ProtectedModal.tsx
@@ -4,39 +4,27 @@ import Modal from "@mui/material/Modal";
 import { useEffect, useState, Dispatch, SetStateAction } from "react";
 import { Box, Typography, Card } from "@mui/material";
 import LockRoundedIcon from "@mui/icons-material/LockRounded";
+import "./ProtectedModal.css";
 
 const style = {
   position: "absolute" as "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  //여기까지 모달 위치
   width: "400px",
   height: "300px",
   bgcolor: "rgb(25, 200, 255)",
   border: 0,
   borderRadius: "10px",
-  // p: 1, // padding,
   color: "white",
-  // padding : "1",
 };
 const style2 = {
-  // position: "absolute" as "absolute",
-  // top: "50%",
-  // left: "50%",
-  // transform: "translate(-50%, -50%)",
-  // width : "300px",
   width: "100",
-  //width는 100하면 꽉 채워지는데
   height: "236px",
-  //height는 100하면 글 줄 수에따라 바뀐다
-  //고정하고 싶어서 px로 정함
   bgcolor: "rgb(18, 163, 255)",
   border: 0,
   borderRadius: "10px",
-  // pt: 1, 이걸하면 box가 내려감
   color: "white",
-  // padding : "1", 안됨 p로 써야됨
   m: 4,
 };
 
@@ -55,14 +43,13 @@ export default function ProtectedModal({
       aria-describedby="enter-password-modal"
     >
       <Box sx={style}>
+              <button className="xbutton">X</button>
         <Box sx={style2}>
-          <div style={{ height: "42px", width: "42px", margin: "auto", padding:"10px" }}>
-            <LockRoundedIcon
-              sx={{ height: "100%", width: "100%", color: "#6c899b" }}
-            />
-            {/* <LockRoundedIcon /> */}
-          </div>
-
+            <div className="prlock">
+              <LockRoundedIcon
+                sx={{ height: "100%", width: "100%", color: "#6c899b" }}
+              />
+            </div>
           {/* <Typography id="protected-room-modal" variant="h6" component="h2">
             Text in a modal
           </Typography>
