211c1a4163f44442ea8f75a2170dea77074b200e
HiHoi <ghtjd5087@gmail.com>
2023-07-31 20:36:54 +0900

    FEAT: Room list context add

---
+++ frontend/context/RoomContext.tsx
@@ -0,0 +1,50 @@
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+enum Mode {
+  PRIVATE = "private",
+  PUBLIC = "public",
+  PROTECTED = "protected",
+}
+
+interface IChatRoom0 {
+  channelIdx: number;
+  owner: string;
+  mode: Mode;
+}
+
+interface RoomContextData {
+  rooms: IChatRoom0[];
+  setRooms: (value: IChatRoom0[]) => void;
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+}
+
+const RoomContext = createContext<RoomContextData>({
+  rooms: [],
+  setRooms: () => {},
+  isOpen: false,
+  setIsOpen: () => {},
+});
+
+export const useRoom = () => {
+  return useContext(RoomContext);
+};
+
+export const RoomProvider = ({ children }: { children: ReactNode }) => {
+  const [rooms, setRooms] = useState<IChatRoom0[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {}, []);
+
+  return (
+    <RoomContext.Provider value={{ rooms, setRooms, isOpen, setIsOpen }}>
+      {children}
+    </RoomContext.Provider>
+  );
+};
