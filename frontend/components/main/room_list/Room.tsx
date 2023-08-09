622fcdc0c84999c04088894a515e664774ef7871
jnkeniaem <jeanne.kim9@gmail.com>
2023-08-09 18:59:13 +0900

    [CHORE] : set up for MAIN_CHAT_2

--- frontend/components/main/room_list/Room.tsx
+++ frontend/components/main/room_list/Room.tsx
@@ -30,31 +30,40 @@ export default function Room({ room, idx }: { room: IChatRoom0; idx: number }) {
       : null;
   };
 
-  // useEffect(() => {
-  //   const ChatEnter = (json) => {
-  //     roomDispatch({ type: "SET_CUR_MEM", value: json.member });
-  //     //channelIdx 안보내줘도 될듯?
-  //   };
-  //   socket.on("chat_enter", ChatEnter);
+  useEffect(() => {
+    const ChatEnter = (json: any) => {
+      console.log("json : ", json);
+      roomDispatch({ type: "SET_CUR_MEM", value: json.member });
+      //channelIdx 안보내줘도 될듯?
+    };
+    socket.on("chat_enter", ChatEnter);
 
-  //   return () => {
-  //     socket.off("chat_enter", ChatEnter);
-  //   };
-  // }, []);
+    return () => {
+      socket.off("chat_enter", ChatEnter);
+    };
+  }, []);
 
   const RoomClick = (room: IChatRoom0) => {
-    socket.emit("chat_enter", { userNickname : "intra_id", userIdx : 3, channelIdx: room.channelIdx, password? : pwRef.current }, (statusCode : number) => {
-    //   if (statusCode가 정상) {
-    if (room.mode === Mode.PROTECTED) handleOpen();
-    else {
-      if (roomState.currentRoom !== room) {
-        roomDispatch({ type: "SET_CURRENTROOM", value: room });
-        roomDispatch({ type: "SET_CUR_MEM", value: mockMemberList0 });
+    socket.emit(
+      "chat_enter",
+      {
+        userNickname: "intra_id",
+        userIdx: 3,
+        channelIdx: room.channelIdx,
+        password: pwRef.current,
+      },
+      (statusCode: number) => {
+        //   if (statusCode가 정상) {
+        if (room.mode === Mode.PROTECTED) handleOpen();
+        else {
+          if (roomState.currentRoom !== room) {
+            roomDispatch({ type: "SET_CURRENTROOM", value: room });
+          }
+          roomDispatch({ type: "SET_ISOPEN", value: true });
+        }
+        //   }
       }
-      roomDispatch({ type: "SET_ISOPEN", value: true });
-    }
-    //   }
-    // });
+    );
   };
 
   return (
