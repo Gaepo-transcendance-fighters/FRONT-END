28653995abfd6e23814b65d3281d187f6724d3e0
jujeon <jwjsjwnsstjd@gmail.com>
2023-08-14 16:31:40 +0900

    [STYLE] clean unused code and comment

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import Layout from "@/components/public/Layout";
-import { useEffect } from "react";
 import { io } from "socket.io-client";
 
 const userId = 3;
-// export const socket = io("http://localhost:4000/chat");
 export const socket = io("http://localhost:4000/chat", {
   query: { userId: userId },
 });
 
 const Page = () => {
-  console.log(socket.id);
   return (
     <>
       <Layout></Layout>
