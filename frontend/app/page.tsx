8bbd4886fda325d89c30313292a0c4b40d1dd595
haryu <haryu@student.42seoul.kr>
2023-08-21 13:40:46 +0900

    WIP

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,9 +5,9 @@ import { io } from "socket.io-client";
 
 const userId =
   typeof window !== "undefined" ? localStorage.getItem("idx") : null;
-export const socket = io("http://localhost:4000/chat", {
+// export const socket = io("http://localhost:4000/chat", {
   // haryu's server
-  // export const socket = io("http://paulryu9309.ddns.net:4000/chat", {
+  export const socket = io("http://paulryu9309.ddns.net:4000/chat", {
   query: { userId: userId },
 });
 
