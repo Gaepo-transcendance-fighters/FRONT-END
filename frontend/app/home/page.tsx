5867b692395e265e4d507f4f1046c436a0b92365
haryu <haryu@student.42seoul.kr>
2023-09-06 21:55:45 +0900

    [FIX] : authState's idx was 0

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -26,6 +26,7 @@ const Page = () => {
   const param = useSearchParams();
   const router = useRouter();
   const [client, setClient] = useState(false);
+  const {authDispatch} = useAuth()
   const { openModal } = useModalContext();
 
   useEffect(() => {
@@ -43,6 +44,7 @@ const Page = () => {
   }, []);
 
   useEffect(() => {
+    authDispatch({type: "SET_ID", value: parseInt(localStorage.getItem('idx')!)})
     setClient(true);
     socket.connect();
     const askInvite = ({
