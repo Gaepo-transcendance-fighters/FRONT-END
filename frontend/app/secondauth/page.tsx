41a3c12072d86cee17000dba2f2ef39f9d85b43c
wochae <officialwochae@gmail.com>
2023-09-11 22:21:31 +0900

    [FIX] : not locStore, but useState #158

--- frontend/app/secondauth/page.tsx
+++ frontend/app/secondauth/page.tsx
@@ -7,7 +7,7 @@ import { main } from "@/type/type";
 import React, { useState } from "react";
 import axios from "axios";
 
-const server_domain = process.env.SERVER_URL_4000;
+const server_domain = process.env.NEXT_PUBLIC_SERVER_URL_4000;
 
 const modalStyle = {
   position: "absolute" as "absolute",
@@ -27,17 +27,18 @@ const SecondAuth = () => {
   const { authState, authDispatch } = useAuth();
   const [inputnumber, setInputNumber] = useState<string>("");
   const router = useRouter();
-
+  const sendUri = `${server_domain}/users/second`;
   const SendMail = async () => {
+    console.log("sendUri", sendUri);
     const response = await axios({
       method: "POST",
-      url: `${server_domain}/users/second`,
-      // url: `http://localhost:4000/users/second`,
+      url: sendUri,
+      headers: {
+        Authorization: "Bearer " + authState.userInfo.authorization,
+      },
       data: {
-        // userIdx: localStorage.getItem("idx"),
-        userIdx: Number(localStorage.getItem("idx")),
-
-        email: localStorage.getItem("email"),
+        userIdx: authState.userInfo.id,
+        email: authState.userInfo.email,
       },
     });
     if (response.status == 200) setBlock(true);
@@ -54,15 +55,15 @@ const SecondAuth = () => {
       // url: `http://localhost:4000/users/second`,
       data: {
         // userIdx: localStorage.getItem("idx"),
-        userIdx: Number(localStorage.getItem("idx")),
+        userIdx: authState.userInfo.id,
         code: Number(inputnumber),
       },
     });
-    if (response.status == 200 && response.data.result.checkTFA === true) {
-      if (!authState.chatSocket) return;
-      authState.chatSocket.emit("set_user_status", {
-        userStatus: { nickname: response.data.nickname },
-      });
+    if (response.status == 200 && response.data.result.checkTFA) {
+      // if (!authState.chatSocket) return;
+      // authState.chatSocket.emit("set_user_status", {
+      //   userStatus: { nickname: response.data.nickname },
+      // });
       return router.push("/home");
     }
     // 라우터 연결 및 localstorage에 2차인증토큰값설정.
