788f008c084c70c94852c44e613bba2ef65da7fc
SiHunLee-1016 <sspkr1782@gmail.com>
2023-09-13 15:30:19 +0900

    [FEAT] : mygamelog

--- frontend/components/main/myprofile/MyGameLog.tsx
+++ frontend/components/main/myprofile/MyGameLog.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Typography } from "@mui/material";
+import { Typography, Box, CardContent } from "@mui/material";
+
 import { Card } from "@mui/material";
 
 import axios from "axios";
@@ -9,7 +10,6 @@ import { useEffect, useState, useRef, useCallback } from "react";
 import { main } from "@/type/type";
 
 import { useUser } from "@/context/UserContext";
-import { useAuth } from "@/context/AuthContext";
 
 const server_domain = process.env.NEXT_PUBLIC_SERVER_URL_4000;
 
@@ -45,10 +45,11 @@ interface GameRecord {
 const MyGameLog = () => {
   const [loading, setLoading] = useState(true);
   const [pageNum, setPageNum] = useState(0);
+
   const [gameRecord, setGameRecord] = useState<GameRecord[]>([]);
+
   const [lastElement, setLastElement] = useState<HTMLDivElement | null>(null);
   const { userState } = useUser();
-  const { authState } = useAuth();
   const observerTarget = useRef(null);
 
   useEffect(() => {
@@ -80,11 +81,13 @@ const MyGameLog = () => {
   const callUser = useCallback(async () => {
     await axios
       .get(
-        `${server_domain}/game/records/userIdx=${authState.userInfo.id}&page=${pageNum}`,
-        // .get(`${server_domain}/game/records/userIdx=${localStorage.getItem("idx")}&page=${pageNum}`,
+        `${server_domain}/game/records/userIdx=${localStorage.getItem(
+          "idx"
+        )}&page=${pageNum}`,
         {
-          headers: {Authorization: "Bearer " + authState.userInfo.authorization,},
-          // headers: {Authorization: "Bearer " + localStorage.getItem("authorization"),},
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("authorization"),
+          },
         }
       )
       .then((res) => {
@@ -115,6 +118,7 @@ const MyGameLog = () => {
           overflowAnchor: "none",
           position: "sticky",
           width: "100%",
+          height: "15vh",
         }}
       >
         {gameRecord.map((gameRecord, i) => {
@@ -126,24 +130,100 @@ const MyGameLog = () => {
                 alignItems: "center",
                 justifyContent: "center",
                 margin: "0px 0 0 0",
-                color: "white",
-                width: "80%",
-                height: "200px",
+                color: "black",
+                width: "90%",
+                height: "70%",
                 // backgroundColor: "#48a0ed",
+                border: "1px solid black",
                 backgroundColor: main.main1,
+                borderRadius: "5px",
               }}
             >
-              <Card
+              {/* 내부에 작은 박스 */}
+              <div
                 style={{
-                  //   backgroundColor: "#86d8f7",
-                  backgroundColor: main.main0,
-                  border: "1px solid black",
-                  width: "80%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  margin: "0px 0 0 0",
+                  color: "black",
+                  width: "90%",
                   height: "80%",
+                  // backgroundColor: "#48a0ed",
+                  backgroundColor: main.main0,
+                  borderRadius: "5px",
                 }}
               >
-                <Typography variant="h6">{gameRecord.type}</Typography>
-              </Card>
+                {/* 내부에서 공간을 나눠야함 */}
+                {/* 왼쪽박스 */}
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    width: "30%",
+                    height: "80%",
+
+                    // backgroundColor: "#48a0ed",
+                    backgroundColor: main.main0,
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      flexDirection: "column",
+                      width: "80%",
+                      height: "40%",
+                      // backgroundColor: "#48a0ed",
+                      backgroundColor: main.main0,
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "0.8rem" }}>
+                      친선/랭크
+                      {gameRecord.type === 0 ? <>Rank</> : <>Normal</>}
+                    </Typography>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      flexDirection: "column",
+                      width: "80%",
+                      height: "40%",
+
+                      // backgroundColor: "#48a0ed",
+                      backgroundColor: main.main0,
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "0.8rem" }}>
+                      {gameRecord.result === 0 ? <>Rank</> : <>Normal</>}
+                    </Typography>
+                  </div>
+                </div>
+                {/* 오른쪽박스 */}
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "70%",
+                    height: "80%",
+
+                    // backgroundColor: "#48a0ed",
+                    backgroundColor: main.main0,
+                  }}
+                >
+                  <Typography sx={{ fontSize: "1rem" }}>
+                    내닉네임 | 점수 : 점수 | 상대닉네임
+                    {userState.nickname} {gameRecord.score}{" "}
+                    {gameRecord.matchUserNickname}
+                  </Typography>
+                </div>
+              </div>
             </div>
           );
         })}
