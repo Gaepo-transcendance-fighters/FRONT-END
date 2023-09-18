44829b3df85eb8429563c35d72dd87a7baaa2f77
jnkeniaem <jeanne.kim9@gmail.com>
2023-09-18 20:43:21 +0900

    [FEAT] : if clicking GameStartButton, i go out from the room i was in

--- frontend/components/game/GameStartButton.tsx
+++ frontend/components/game/GameStartButton.tsx
@@ -4,12 +4,38 @@ import { Button } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useRoom } from "@/context/RoomContext";
+import { useAuth } from "@/context/AuthContext";
+import secureLocalStorage from "react-secure-storage";
+import { ReturnMsgDto } from "@/type/RoomType";
 
 const GameStartButton = () => {
   const router = useRouter();
   const { roomState, roomDispatch } = useRoom();
+  const { authState } = useAuth();
 
   const onClick = () => {
+    console.log("hihi");
+    if (roomState.currentRoom?.channelIdx) {
+      authState.chatSocket!.emit(
+        "chat_goto_lobby",
+        {
+          channelIdx: roomState.currentRoom!.channelIdx,
+          userIdx: parseInt(secureLocalStorage.getItem("idx") as string),
+        },
+        (ret: ReturnMsgDto) => {
+          console.log("GameStartButton : ", ret.msg);
+          if (ret.code === 200) {
+            roomDispatch({ type: "SET_IS_OPEN", value: false });
+            roomDispatch({ type: "SET_CUR_ROOM", value: null });
+          } else if (ret.code === 400) {
+            roomDispatch({ type: "SET_IS_OPEN", value: false });
+            roomDispatch({ type: "SET_CUR_ROOM", value: null });
+          } else {
+            console.log("GameStartButton : ", ret.msg);
+          }
+        }
+      );
+    }
     router.push("./game");
   };
 
