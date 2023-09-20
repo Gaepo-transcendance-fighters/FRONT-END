6f3c8bcb3fc392c4fe2b18f6c8e9c46c87b1d109
HiHoi <ghtjd5087@gmail.com>
2023-09-20 20:37:41 +0900

    fix

--- frontend/components/game/ingame/PingPong.tsx
+++ frontend/components/game/ingame/PingPong.tsx
@@ -217,16 +217,23 @@ const PingPong = () => {
         { userIdx: parseInt(secureLocalStorage.getItem("idx") as string) },
         (res: ReturnMsgDto) => {
           console.log(res);
-          if (
-            data.gameStatus === EGameStatus.END ||
-            data.gameStatus === EGameStatus.JUDGE
-          ) {
-            gameDispatch({ type: "SCORE_RESET" });
-            router.replace("/gameresult");
-          }
+          // if (
+          //   data.gameStatus === EGameStatus.END ||
+          //   data.gameStatus === EGameStatus.JUDGE
+          // ) {
+          //   gameDispatch({ type: "SCORE_RESET" });
+          //   router.replace("/gameresult");
+          // }
           if (res.code === 200) {
             gameDispatch({ type: "A_SCORE", value: data.userScore1 });
             gameDispatch({ type: "B_SCORE", value: data.userScore2 });
+            if (
+              data.gameStatus === EGameStatus.END ||
+              data.gameStatus === EGameStatus.JUDGE
+            ) {
+              gameDispatch({ type: "SCORE_RESET" });
+              router.replace("/gameresult");
+            }
           }
         }
       );
