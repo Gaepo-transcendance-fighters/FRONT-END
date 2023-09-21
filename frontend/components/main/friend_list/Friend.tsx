f95c0fa5f43d4189d8d9fb4ee5857055a8abb4a6
jujeon <jwjsjwnsstjd@gmail.com>
2023-09-22 00:24:15 +0900

    [FIX] onGaming you can see green light

--- frontend/components/main/friend_list/Friend.tsx
+++ frontend/components/main/friend_list/Friend.tsx
@@ -36,7 +36,7 @@ const Friend = ({ prop }: { prop: IFriend }) => {
             </Typography>
           </Tooltip>
           <Stack direction={"row"} alignItems={"center"}>
-            {prop.isOnline === IOnlineStatus.ONLINE ? loginOn : loginOff}
+            {prop.isOnline === IOnlineStatus.ONLINE || prop.isOnline === IOnlineStatus.GAMEING ? loginOn : loginOff}
             <FriendProfile prop={prop as IFriend} />
           </Stack>
         </Stack>
